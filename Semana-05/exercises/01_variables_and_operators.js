70b1e319c3c107f946265ae33f0dba48f524f4bf
Sofia Baute <91097658+SoofiBaute@users.noreply.github.com>
2022-04-15 12:39:44 -0300

    BaSP-2022: first exercise

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,16 @@
+/*variables and operators*/
+
+// a)
+var num1=10, num2=5, suma;
+suma= num1+num2;
+console.log('The sum of the numbers is: ', suma);
+
+// b)
+var frase1= 'hello ', frase2= 'how are you?', frase3;
+frase3= frase1+frase2;
+console.log(frase3);
+
+// c)
+var palabra1= 'hello', palabra2='how are you?', longitud;
+longitud= palabra1.length + palabra2.length;
+console.log('The sum of its length is: ', longitud);
