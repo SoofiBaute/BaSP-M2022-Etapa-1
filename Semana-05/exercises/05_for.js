2a4d924bc6fc695cbc1302203bf521bd36b69c38
Sofia Baute <91097658+SoofiBaute@users.noreply.github.com>
2022-04-16 00:44:48 -0300

    BaSP-2022: Add exercise 05

---
+++ Semana-05/exercises/05_for.js
@@ -0,0 +1,36 @@
+/*For*/
+
+// a) Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras.
+
+var frutas = ['pera', 'uva', 'pomelo', 'naranja', 'manzana'];
+
+for(var i=0; i<5; i++){
+    alert(frutas[i]);
+}
+
+// b) Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.
+
+for(var i=0; i<5; i++){
+    frutas[i] = (frutas[i].substring(0,1).toUpperCase()) + frutas[i].substring(1,frutas[i].length);
+    alert(frutas[i]);
+}
+
+// c) Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa.
+
+var sentence =" ";
+
+for(var i=0; i<5; i++){
+    sentence += " ";
+    sentence += frutas[i];
+}
+alert(sentence);
+
+// d) Crear una array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número de la repetición, es decir que al final de la ejecución del bucle for debería haber 10 elementos dentro del array, desde el número 0 hasta al número 9. Mostrar por la consola del navegador el array final (utilizar console.log)
+
+var arr = [];
+
+for(var i=0; i < 10; i++){
+    arr [i]= i; 
+}
+console.log(arr);
+
