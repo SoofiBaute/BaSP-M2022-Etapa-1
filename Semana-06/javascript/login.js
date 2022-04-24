be3b2c91c76e8428d9edbc950654b36f26c2443b
Sofia Baute <91097658+SoofiBaute@users.noreply.github.com>
2022-04-23 23:02:54 -0300

    BaSP-2022: add login js

---
+++ Semana-06/javascript/login.js
@@ -0,0 +1,13 @@
+/*var login = document.getElementById("login");
+
+login.addEventListener("click", (e)=>{
+    alert("Ingresaste correctamente");
+    console.log(e);
+});
+
+var inputName = document.getElementById('inputName');
+
+inputName.addEventListener("click", e =>{
+  
+    inputName.focus();
+});*/
