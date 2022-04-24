2b26734b61b49f3563ffc9bae43cf9bb6d181d30
Sofia Baute <91097658+SoofiBaute@users.noreply.github.com>
2022-04-24 18:08:37 -0300

    BaSP-2022: validations for email to Login

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -1,13 +1,62 @@
-/*var login = document.getElementById("login");
+var inputEmail = document.getElementById('input-Email');
+var inputPassword = document.getElementById('input-password');
+var validation = document.getElementById('emailOK');
+var login = document.getElementById('login');
+var dataSendEmail = document.getElementById('dataSend-email');
+var dataSendPassword = document.getElementById('dataSend-password');
+var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+var passwordRegex = 12;
 
-login.addEventListener("click", (e)=>{
-    alert("Ingresaste correctamente");
-    console.log(e);
-});
+//EVENTS
+inputEmail.addEventListener("focus", myFocusFunction,true);
+inputEmail.addEventListener("blur", myBlurFunction,true);
+inputPassword.addEventListener("focus", myFocusFunction,true);
+inputPassword.addEventListener("blur", myBlurFunction,true);
+login.addEventListener("click", send);
 
-var inputName = document.getElementById('inputName');
 
-inputName.addEventListener("click", e =>{
-  
-    inputName.focus();
-});*/
+//FUNCTIONS
+function send(){
+    console.log("entre a send")
+    dataSendEmail.style.visibility= "visible"
+    dataSendEmail.innerText = `
+        DATOS CORRECTAMENTE INGRESADOS`;
+        /*email:  + email + `
+        Password
+        `+ password*/
+}
+
+function myFocusFunction() {
+    inputEmail.style.backgroundColor = "white";
+    inputPassword.style.backgroundColor = "white";
+}
+
+function myBlurFunction() {
+    
+    email = document.getElementById('input-Email').value;
+    password = document.getElementById('input-password').value;
+
+    //Validation of email
+    if(emailRegex.test(email)){
+    inputEmail.style.border = "1px solid #4E944F";
+    validation.style.color = "#4E944F";
+    validation.innerText = "correct";
+    } else {
+        inputEmail.style.border = "solid 1px #B20600";
+        validation.style.color = "#B20600";
+        validation.innerText = "incorrect";
+        //alert("complete the mail correctly"); 
+    }
+
+    /*Validation of password
+    if(passwordRegex.test(password)){
+        inputPassword.style.border = "1px solid #4E944F";
+        validation.style.color = "#4E944F";
+        validation.innerText = "correct";
+        } else {
+            inputPassword.style.border = "solid 1px #B20600";
+            validation.style.color = "#B20600";
+            validation.innerText = "incorrect";
+            //alert("complete the mail correctly"); 
+        }*/
+}   
